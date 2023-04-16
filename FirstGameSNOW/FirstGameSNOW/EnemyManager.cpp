79ecafb7e8579807d0975f29b07d3fee426ac450
gamer013NL <Tygo.vanGorp@outlook.com>
2023-04-16 12:05:31 +0200

    add explenation to random int gen for enemies

--- FirstGameSNOW/FirstGameSNOW/EnemyManager.cpp
+++ FirstGameSNOW/FirstGameSNOW/EnemyManager.cpp
@@ -22,7 +22,6 @@ void EnemyManager::CheckDup(Entity** Entitys, int size)
 			if (Entitys[i]->EntityLocX == Entitys[j]->EntityLocX 
 				&&
 				Entitys[i]->EntityLocY == Entitys[j]->EntityLocY) {
-				std::cout << i << " and " << j << " are the in the same location" << std::endl;
 				GenerateRandomInt(*Entitys[i]);
 				CheckDup(Entitys, size);
 			}
@@ -35,10 +34,10 @@ void EnemyManager::GenerateRandomInt(Entity& Entity)
 {
 	int min = 0;
 	int max = Game::ScreenSize/Game::gridSize;
-	std::random_device rd;
-	std::mt19937 rng(rd());
-	std::uniform_int_distribution<int> dist(min, max);
-	int tmpRand1 = dist(rng);
+	std::random_device rd; //generate seed
+	std::mt19937 rng(rd()); //initialize seed
+	std::uniform_int_distribution<int> dist(min, max); //used to define a uniform distribution of integers between the min and max values
+	int tmpRand1 = dist(rng); //generates a random number
 	int tmpRand2 = dist(rng);
 	Entity.setLoc(tmpRand1 * Game::gridSize, tmpRand2 * Game::gridSize);
 }
